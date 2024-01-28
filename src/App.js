f827db0f30b66d760d2b0266a05bdd1ab259d684
odionjulius7 <odionjulius7@gmail.com>
2024-01-28 20:44:13 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Properties from "./Pages/Properties";
+import Projects from "./Pages/Projects";
+import Blogs from "./Pages/Blogs";
+import AboutUs from "./Pages/AboutUs";
+import PropertyRequested from "./Pages/PropertyRequested";
+import PropertySelected from "./Pages/PropertySelected";
+import SelectedProject from "./Pages/SelectedProject";
+import SelectedBlog from "./Pages/SelectedBlog";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/properties" element={<Properties />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/property-reqed" element={<PropertyRequested />} />
+        <Route path="/property-selected" element={<PropertySelected />} />
+        <Route path="/selected-project" element={<SelectedProject />} />
+        <Route path="/selected-blog" element={<SelectedBlog />} />
+      </Routes>
+    </Router>
   );
 }
 
