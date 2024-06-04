17560a5789fef9cf87b247a006b941082c246b65
Odion Julius <odionjulius7@gmail.com>
2024-06-04 19:38:44 +0100

    url

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,2 +1,3 @@
-export const base_url = "https://jea-backend.onrender.com/api/";
+export const base_url = "https://joetoniruest.com/api/";
+// export const base_url = "https://jea-backend.onrender.com/api/";
 // https://jea-backend.onrender.com/api/property/all
