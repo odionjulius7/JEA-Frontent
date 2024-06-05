fe1aae68485f567abfabd4463b30288fdd15f5ad
Odion Julius <odionjulius7@gmail.com>
2024-06-05 06:20:55 +0100

    digital Occean

--- src/utils/accordion.jsx
+++ src/utils/accordion.jsx
@@ -1,9 +1,10 @@
 import { HiShieldCheck } from "react-icons/hi";
 import { MdCancel, MdAnalytics } from "react-icons/md";
+// yet
 const data = [
   {
-    icon: <HiShieldCheck/>,
-    heading: "Best interest rates on the market",
+    icon: <HiShieldCheck />,
+    heading: "Best interest rates on the marketer",
     detail:
       "Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim minim esse tempor laborum consequat esse adipisicing eu reprehenderit enim.",
   },
